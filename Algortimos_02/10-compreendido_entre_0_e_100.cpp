9681b53bfd210dc75a2c55ec7c31f07eded54624
Ricardo Martins <ricardomart6373@gmail.com>
2020-08-13 17:54:36 -0300

    Update 10-compreendido_entre_0_e_100.cpp

--- Algortimos_02/10-compreendido_entre_0_e_100.cpp
+++ Algortimos_02/10-compreendido_entre_0_e_100.cpp
@@ -12,7 +12,7 @@ int main(){
 	if(numero >= 0 && numero <= 100){
 		printf("\nO número digitado estar compreendido entre 0 e 100.");
 	}else{
-		printf("\nO número digitado não estar compreendido entre 0 e 100.")
+		printf("\nO número digitado não estar compreendido entre 0 e 100.");
 	}
 	
 	return 0;
